198fc210c2fad1412f027eda7c5cef8d287f20ab
SAMIM-ANSARI02 <ijsamimansari02@gmail.com>
2023-04-21 10:46:10 +0530

    change

--- Desktop/react assignment/invitation/invitation/src/App.js
+++ Desktop/react assignment/invitation/invitation/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <div>Subject : Birthday party Invitation</div>
+ <div>To : jaGdish@gmail.com</div> 
+  <div>Hi , jaGdish</div>  
+  
+  <div>I am having a birthday party next Friday at my Home . Would you like to come ? It will be fun . Lots of people from my school are coming . you know some of them - Ritu , Saurabh , Kartik .
+  
+  My house is behind our school , near Green field Avenue .
+  </div> 
+  I hope you will come and see you soon .
+  
+  from ,
+  <div>JaGdish</div> 
+  </>
+
+      
+  
+  )
 }
 
-export default App;
+export default App
+
